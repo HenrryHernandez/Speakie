96b0eb0497d438cc1456a98ba97905e308a11f00
Henrry Hernandez <jenrruhndz@gmail.com>
2023-01-05 19:10:32 -0700

    style (menu-bar): change color when scrolling over menu bar height

--- src/script.js
+++ src/script.js
@@ -15,3 +15,22 @@ const openMenu = () => {
 
 const openMenuBtn = document.getElementById("open-menu");
 openMenuBtn.addEventListener("click", openMenu);
+
+const changeHeaderColor = () => {
+  const header = document.getElementById("header");
+  const bars = document.getElementById("open-menu");
+
+  const headerSize = 64;
+
+  if (this.scrollY >= headerSize) {
+    header.classList.add("bg-one");
+    bars.classList.remove("text-one");
+    bars.classList.add("text-white");
+  } else {
+    header.classList.remove("bg-one");
+    bars.classList.remove("text-white");
+    bars.classList.add("text-one");
+  }
+};
+
+window.addEventListener("scroll", changeHeaderColor);
